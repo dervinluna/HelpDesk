1b5b5602649740398c1ed62441d1072eb6066605
dervinluna <dhernandezl24@miumg.edu.gt>
2023-05-11 19:00:40 -0600

    creamos la clase usuario

---
+++ ProyectoHelpDesk/Back/Solicitud.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoHelpDesk.Back
+{
+    internal class Solicitud : Usuario
+    {
+        public int ticket { get; set; }
+        public string estado { get; set; }
+        public string descripcion { get; set; }
+        public Solicitud() { }
+
+        public Solicitud(int ticket, string estado, string descripcion, int idCliente, int? idTecnico)
+        {
+            this.ticket = ticket;
+            this.estado = estado;
+            this.descripcion = descripcion;
+            this.id = idCliente;
+            //validamos debido a que en la generacion de la solicitud no vendra ese parametro
+            if (idCliente != null)
+            {
+                this.id = idTecnico;
+            }
+
+        }
+        public bool Generar(int ticket, string estado, string descripcion, int idCliente, int? idTecnico)
+        {
+            Solicitud solicitud = new Solicitud();
+            return true;
+        }
+    }
+}
--- ProyectoHelpDesk/Back/Usuario.cs
+++ ProyectoHelpDesk/Back/Usuario.cs
@@ -8,5 +8,23 @@ namespace ProyectoHelpDesk.Back
 {
     internal class Usuario
     {
+
+        //Creamos los Atributos de la Superclase
+        public int? id { get; set; }
+        public string nombre { get; set; }
+        public string usuario { get; set; }
+        public string contraseña { get; set; }
+        public string tipo { get; set; } //(JefeIt,Cliente,Tipo)
+        //constructor sin parametros 
+        public Usuario() { }
+        //constructor con parametros
+        public Usuario(int? id, string nombre, string usuario, string contraseña, string tipo)
+        {
+            this.id = id;
+            this.nombre = nombre;
+            this.usuario = usuario;
+            this.contraseña = contraseña;
+            this.tipo = tipo;
+        }
     }
 }
