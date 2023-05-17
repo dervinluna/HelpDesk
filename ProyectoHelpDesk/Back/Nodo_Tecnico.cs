d3a64375bcb91800f562911f534ea1805cdcc3ad
daenn1431 <jalvarezm27@miumg.edu.gt>
2023-05-16 22:03:31 -0600

    Se creo la clase Nodo_Tecnico

---
+++ ProyectoHelpDesk/Back/Nodo_Tecnico.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoHelpDesk.Back
+{
+    internal class Nodo
+    {
+        public Tecnico Informacion { get; set; }
+        public Nodo Liga { get; set; }
+
+        public Nodo()
+        {
+            Informacion = null;
+            Liga = null;
+        }
+
+        public Nodo(Tecnico informacion)
+        {
+            Informacion = informacion;
+            Liga = null;
+        }
+
+        public override string ToString()
+        {
+            return $"{Informacion.nombre} ({Informacion.idTecnico})";
+        }
+    }
+}
